718c4491999d8bfb5f987bed1ac5ce47c2c7ffe4
hiroyuki <hiroyuki@mbp1.local>
2016-12-25 18:03:40 +0900

    fix animation of LocationPickCounter

--- src/js/containers/LocationPickCounter.jsx
+++ src/js/containers/LocationPickCounter.jsx
@@ -1,8 +1,18 @@
 import React, { Component, PropTypes } from 'react';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { connect } from 'react-redux'
 
 class LocationPickCounter extends Component {
+
+  componentWillUpdate() {
+    // TODO:optional: It might be a better way to add/remove class like jQuery
+    this.counterPulse.className = 'a-pick-counter';
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.counterPulse.className = '';
+    }, 1000);
+  }
 
   render() {
     const { items } = this.props;
@@ -12,19 +22,23 @@ class LocationPickCounter extends Component {
     }, 0);
     const counter = <span key={count}>{count}</span>;
 
-    // <ReactCSSTransitionGroup transitionName="sample"
-    //                     transitionEnterTimeout={500}
-    //                     transitionLeave={false}>
-    //   {counter}
-    // </ReactCSSTransitionGroup>
-
     return (
       <div className="locationPickCounter">
         {counter}
-        <div className="animation-pulse"></div>
+        <div ref={(el) => { this.counterPulse = el; }} className=""></div>
       </div>
     )
   }
+
+  // // INFO: standard react animation feature is below.
+  // import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+  //
+  // <ReactCSSTransitionGroup transitionName="sample"
+  //                     transitionEnterTimeout={500}
+  //                     transitionLeave={false}>
+  //   <div ref={(el) => { this.counterPulse = el; }} className=""></div>
+  // </ReactCSSTransitionGroup>
+
 };
 
 function mapStateToProps(state, ownProps) {
