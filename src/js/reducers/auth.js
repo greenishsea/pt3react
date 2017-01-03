049b1fd80e8ce8dcff906a4b0bc9a73c1b74da82
hiroyuki <hiroyuki@mbp1.local>
2017-01-04 01:30:10 +0900

    (format) add braces to switch's case blocks

--- src/js/reducers/auth.js
+++ src/js/reducers/auth.js
@@ -1,50 +1,55 @@
 import {
-    AUTH_LOGIN_USER_REQUEST,
-    AUTH_LOGIN_USER_SUCCESS,
-    AUTH_LOGIN_USER_FAILURE,
-    AUTH_LOGOUT_USER
+  AUTH_LOGIN_USER_REQUEST,
+  AUTH_LOGIN_USER_SUCCESS,
+  AUTH_LOGIN_USER_FAILURE,
+  AUTH_LOGOUT_USER
 } from '../actions';
 import * as initialState from './initialState';
 
 export function auth(state = initialState.auth, action) {
   switch (action.type) {
-    case AUTH_LOGIN_USER_REQUEST:
-        return Object.assign({}, state, {
-            isAuthenticating: true,
-            statusText: null
-        });
-    case AUTH_LOGIN_USER_SUCCESS:
-        if (action.payload.hasError) {
-          return Object.assign({}, state, {
-              isAuthenticating: false,
-              isAuthenticated: false,
-              token: null,
-              statusText: 'Authentication Error:'
-          });
-        } else {
-          return Object.assign({}, state, {
-              isAuthenticating: false,
-              isAuthenticated: true,
-              token: action.payload.token,
-              statusText: 'You have been successfully logged in.'
-          });
-        }
-    case AUTH_LOGIN_USER_FAILURE:
+    case AUTH_LOGIN_USER_REQUEST: {
+      return Object.assign({}, state, {
+          isAuthenticating: true,
+          statusText: null
+      });
+    }
+    case AUTH_LOGIN_USER_SUCCESS: {
+      if (action.payload.hasError) {
         return Object.assign({}, state, {
             isAuthenticating: false,
             isAuthenticated: false,
             token: null,
-            userName: null,
-            statusText: `Authentication Error:`
+            statusText: 'Authentication Error:'
         });
-    case AUTH_LOGOUT_USER:
+      } else {
         return Object.assign({}, state, {
-            isAuthenticated: false,
-            token: null,
-            userName: null,
-            statusText: 'You have been successfully logged out.'
+            isAuthenticating: false,
+            isAuthenticated: true,
+            token: action.payload.token,
+            statusText: 'You have been successfully logged in.'
         });
-    default:
+      }
+    }
+    case AUTH_LOGIN_USER_FAILURE: {
+      return Object.assign({}, state, {
+          isAuthenticating: false,
+          isAuthenticated: false,
+          token: null,
+          userName: null,
+          statusText: `Authentication Error:`
+      });
+    }
+    case AUTH_LOGOUT_USER: {
+      return Object.assign({}, state, {
+          isAuthenticated: false,
+          token: null,
+          userName: null,
+          statusText: 'You have been successfully logged out.'
+      });
+    }
+    default: {
       return state;
+    }
   }
 }
--- src/js/reducers/modal.js
+++ src/js/reducers/modal.js
@@ -3,20 +3,23 @@ import * as initialState from './initialState';
 
 export function modal(state = initialState.modal, action) {
   switch (action.type) {
-    case MODAL_SHOW:
+    case MODAL_SHOW: {
       return {
         ...state,
         show: true,
         content: action.payload.content,
       };
-    case MODAL_HIDE:
+    }
+    case MODAL_HIDE: {
       return {
         ...state,
         show: false,
         // content: {}, // INFO: commented out for perfomance
       };
-    default:
+    }
+    default: {
       return state;
+    }
   }
 }
 
